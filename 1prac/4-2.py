98f2ae35ebf2e26de7ccb6bc536a36765f2a79e1
adorabledeadsystem <39247640+adorabledeadsystem@users.noreply.github.com>
2022-09-16 15:27:11 +0300

    Update 4-2.py

--- 1prac/4-2.py
+++ 1prac/4-2.py
@@ -29,3 +29,31 @@ plt.xlabel("X")
 plt.ylabel("Y")
 plt.grid()
 plt.show();
+
+
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import math as m
+from scipy.integrate import simps
+from numpy import trapz as tr
+
+
+def fun(x: float) -> float:
+    return abs(m.cos(x * m.pow(m.e, m.cos(x) + m.log(x + 1, m.e))))
+
+
+xs = list(range(0, 11, 1))
+ys = []
+for i in xs:
+    ys.append(fun(i))
+
+plt.plot(xs, ys)
+plt.fill_between(xs, ys)
+print(tr(ys))
+plt.title("Graphic")
+plt.xlabel("X")
+plt.ylabel("Y")
+plt.grid()
+plt.show();
