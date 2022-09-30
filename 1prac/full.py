4aeedb4bbfe2ae216efbbfa64e991890661b92e4
adorabledeadsystem <39247640+adorabledeadsystem@users.noreply.github.com>
2022-09-30 11:20:01 +0300

    Update full.py

--- 1prac/full.py
+++ 1prac/full.py
@@ -184,24 +184,21 @@ def fun(x:float) -> float:
     p2 = abs(1 - m.pow(m.sin(x), 3))
     return p1/p2 + loga
 mas = []
+
 for i in range(1,11):
     mas.append(fun(i))
-    
-plt.plot(range(1, 11), mas)
+
+mas_slice = mas[0:10]
+slice_fig, slice_ax = plt.subplots(figsize=(10,7))
+slice_ax.scatter(x = range(0, len(mas_slice)), y = mas_slice, color = "r") 
+plt.plot(range(0,10), mas)
+
     
 plt.title("Graphic")
 plt.xlabel("X")
 plt.ylabel("Y")
 plt.grid()
 plt.show()
-
-mas_slice = mas[0:5]
-slice_fig, slice_ax = plt.subplots(figsize=(12,8))
-slice_ax.scatter(x = range(0, len(mas_slice)), y = mas_slice)
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.grid()
-plt.show();
 
 
 
